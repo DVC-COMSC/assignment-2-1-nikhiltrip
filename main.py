56ce6454a99b430c0881ee2231885f6add854787
Nikhil Tripathi <nikhiltrip1@gmail.com>
2023-06-24 21:14:28 -0700

    create main.py

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-
+    males=int(input("Number of males:"))
+    females=int(input("Number of females:"))
+    total=males+females
+    m_perc=males/total*100
+    f_perc=females/total*100
+    print("The total number of students: \t\t{}".format(total))
+    print("The number of males and females \t{} \t{}".format(males,females))
+    print("The percentage of males and females \t{:.2f}% \t{:.2f}%".format(m_perc,f_perc))
     """
+    
     ########################################
     # Do not delete the return statement
     ########################################
